fb7d5cdab296510691fe70b33ba2c06ec88afa63
Nikita Stoichev <120448742+Nastoychev-1@users.noreply.github.com>
2022-12-26 19:16:03 +0300

    Add solution for primes again

--- homework_01/main.py
+++ homework_01/main.py
@@ -32,7 +32,7 @@ def filter_numbers(numbers_list, filter_type):
     """
     if filter_type == ODD:
         return [number for number in numbers_list if number % 2 != 0]
-    if filter_type == EVEN
+    if filter_type == EVEN:
         return [number for number in numbers_list if number % 2 == 0]
 
 def is_prime(numbers_list, filter_type):
