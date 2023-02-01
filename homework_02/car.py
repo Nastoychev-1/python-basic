da3c52c1f14ac0230acb5c6163a49d79aeafe0d9
Nikita Stoichev <120448742+Nastoychev-1@users.noreply.github.com>
2023-02-01 17:29:39 +0300

    Add homework_2

--- homework_02/car.py
+++ homework_02/car.py
@@ -1,3 +1,16 @@
 """
 создайте класс `Car`, наследник `Vehicle`
 """
+import homework_02.engine
+from homework_02.base import Vehicle
+from engine import Engine
+
+
+class Car(Vehicle):
+    engine: str
+
+    def set_engine(self, engine):
+        self.engine = Engine(volume=5, pistons=5)
+
+        # self.volume = Engine.volume
+        # self.pistons = Engine.pistons
