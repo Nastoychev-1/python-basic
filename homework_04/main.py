6415ede6eac2f0ecf91a5587434da21a522f1263
Nikita Stoichev <120448742+Nastoychev-1@users.noreply.github.com>
2023-03-09 13:18:19 +0600

    Add homework_04

--- homework_04/main.py
+++ homework_04/main.py
@@ -22,10 +22,32 @@ async def create_user(session: AsyncSession, data_users) -> list[User]:
     users = [User(name=i.get('name'), username=i.get('username'), email=i.get('email')) for i in data_users]
     session.add_all(users)
     await session.commit()
+    return users
+
+
+async def create_post(session: AsyncSession, data_posts):
+    posts = [Post(user_id=i.get('userId'), title=i.get('title'), body=i.get('body')) for i in data_posts]
+    session.add_all(posts)
+    await session.commit()
+    return posts
+
+
+async def async_main():
+    await create_tables()
+    result_users, result_posts = await asyncio.gather(fetch_users_data(USERS_DATA_URL),
+                                                      fetch_posts_data(POSTS_DATA_URL))
+    async with Session() as session:
+        await create_user(session, result_users)
+    async with Session() as session:
+        await create_post(session, result_posts)
 
 
 def main():
-    pass
+    asyncio.get_event_loop().run_until_complete(async_main())
+
+
+if __name__ == "__main__":
+    main()
 
 
 if __name__ == "__main__":
