caa128edb1238bd5a1b80180ee449021d39af20f
Nikita Stoichev <120448742+Nastoychev-1@users.noreply.github.com>
2023-05-05 18:14:16 +0600

    homework_07

--- fitness/mainapp/models.py
+++ fitness/mainapp/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
+
 # Create your models here.
+# Фитнес приложение в котором распредленны разные группы мышц по отдельным группам с упражнениям
+
+
+class Exercise(models.Model):
+    objects = None
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(unique=True, max_length=50)
+    exercises = models.ManyToManyField(Exercise)
+
+    def __str__(self):
+        return self.name
+
+
+class Part_of_the_body(models.Model):
+    name = models.CharField(max_length=50)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.name
+
+
+class Card(models.Model):
+    text = models.TextField(blank=True, null=True)
+    part_of_the_body = models.ManyToManyField(Part_of_the_body)
+
+    def __str__(self):
+        return self.part_of_the_body.name
--- fitness/mainapp/views.py
+++ fitness/mainapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Exercise
+
 
 # Create your views here.
+def index_view(request):
+    exerises = Exercise.objects.all()
+    return render(request, 'mainapp/index.html', {'exerises': exerises})
